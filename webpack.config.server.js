bd31c1882e655e41c281b7de75c58e9b08952248
Sun-Deep <sundeepp29@gmail.com>
2020-07-26 10:41:41 +0545

    user model added

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,4 +1,8 @@
-const nodeExternals = require("webpack-node-externals");
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const CURRENT_WORKING_DIR = process.cwd()
+
+
 const config = {
   name: "server",
   entry: [path.join(CURRENT_WORKING_DIR, "./server/server.js")],
